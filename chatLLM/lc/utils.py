36a30014bd8eff6a5939bee4360b4e7b399e016a
krishpranav <krisna.pranav@gmail.com>
2023-06-11 18:46:35 +0530

    chatLLM: lc[utils]

--- chatLLM/lc/utils.py
+++ chatLLM/lc/utils.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
-
 from typing import Any
 from chatLLM.types import LLMSettings
 from typing import List, Optional
+
 
 def run_langchain_agent(agent: Any, input_str: str):
     if hasattr(agent, "input_keys"):
@@ -10,10 +9,30 @@ def run_langchain_agent(agent: Any, input_str: str):
         raw_res = agent({input_key: input_str})
     else:
         raw_res = agent(input_str)
-    
+
     if hasattr(agent, "output_keys"):
         output_key = agent.output_keys[0]
     else:
         output_key = None
 
     return raw_res, output_key
+
+
+def get_llm_settings(llm, stop: Optional[List[str]] = None):
+    if llm.__class__.__name__ == "OpenAI":
+        return LLMSettings(
+            model_name=llm.model_name,
+            stop=stop,
+            temperature=llm.temperature,
+            max_tokens=llm.max_tokens,
+            top_p=llm.top_p,
+            frequency_penalty=llm.frequency_penalty,
+            presence_penalty=llm.presence_penalty,
+        )
+    elif llm.__class__.__name__ == "ChatOpenAI":
+        return LLMSettings(
+            model_name=llm.model_name,
+            stop=stop,
+        )
+    else:
+        return None
